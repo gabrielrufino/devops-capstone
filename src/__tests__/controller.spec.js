cfed57b71c810f74f5cb4860598c74de64bb483f
Gabriel Rufino <contato@gabrielrufino.com>
2021-09-11 11:27:10 -0300

    Test for controller

---
+++ src/__tests__/controller.spec.js
@@ -0,0 +1,15 @@
+const controller = require('../controller')
+
+describe('Tests for controller', () => {
+  it('Should send the correct response body', () => {
+    const request = {}
+    const response = { send: jest.fn() }
+    const next = jest.fn()
+
+    controller(request, response, next)
+
+    expect(response.send).toBeCalledWith({
+      message: 'Hello, capstone!'
+    })
+  })
+})
